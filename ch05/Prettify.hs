47071a15c8328b906691c49322e4e894a839af51
Pavel Rybintsev <pavelryb86@gmail.com>
2017-06-22 00:06:12 +0300

    Started implementing with compiling every step

---
+++ ch05/Prettify.hs
@@ -0,0 +1,17 @@
+module Prettify
+  (
+    Doc,
+    text,
+    double,
+  ) where 
+
+import SimpleJSON
+
+data Doc = ToBeDefined
+         deriving (Show)
+
+text :: String -> Doc
+text str = undefined
+
+double :: Double -> Doc
+double num = undefined
--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -1,15 +1,47 @@
 import Numeric
 
-renderJValue :: JValue -> Doc
-renderJValue :: (JBool True)  = text "true"
-renderJValue :: (JBool False) = text "false"
-renderJValue :: JNull         = text "null"
-renderJValue :: (JString str) = string str
+import Numeric (showHex)
+import Data.Char (ord)
+import Data.Bits (shiftR, (.&.))
+
+import SimpleJSON (JValue(..))
+import Prettify
 
 
 string :: String -> Doc
 string = enclose '"' '"' . hcat . map oneChar
 
+enclose :: Char -> Char -> Doc -> Doc
+enclose left right x = char left <> x <> char right
+
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+              Just r -> text r
+              Nothing | mustEscape c -> hexEscape c
+                      | otherwise    -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\',b])
+
+hexEscape c = undefined
+
+{-|
+renderJValue :: JValue -> Doc
+renderJValue :: (JBool True)  = text "true"
+renderJValue :: (JBool False) = text "false"
+renderJValue :: JNull         = text "null"
+renderJValue :: (JString str) = string str
 
 enclose :: Char -> Char -> Doc -> Doc
 enclose :: left right x = char left <> x <> char right
@@ -50,3 +82,5 @@ hexEscape :: Char -> Doc
 hexEscape c | d < 0x10000 = smallHex d
             | otherwise   = astral (d - 0x10000)
   where d = ord c
+-}
+
